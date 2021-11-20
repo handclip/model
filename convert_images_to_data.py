f2f089124f94cafd72f6b7778ad5b622804fbed0
Elon Gliksberg <elon@egk.local>
2021-11-20 11:04:02 +0200

    Renamed a function.

--- convert_images_to_data.py
+++ convert_images_to_data.py
@@ -14,7 +14,7 @@ mp_hands = mp.solutions.hands
 hands = mp_hands.Hands()
 
 
-def images_to_landmarks(image_paths: List[str], model_class: ModelClass):
+def images_to_data(image_paths: List[str], model_class: ModelClass):
     total_hand_landmarks = []
 
     for path in image_paths:
@@ -23,7 +23,8 @@ def images_to_landmarks(image_paths: List[str], model_class: ModelClass):
         result = hands.process(frame_rgb)
         if result.multi_hand_landmarks:
             for hand_landmarks in result.multi_hand_landmarks:
-                hand_landmarks = [(coord.x, coord.y, coord.z) for coord in hand_landmarks.landmark]
+                hand_landmarks = [(coord.x, coord.y, coord.z)
+                                  for coord in hand_landmarks.landmark]
                 total_hand_landmarks.append(hand_landmarks)
 
     np.save(dataset.get_dataset_path(model_class), total_hand_landmarks)
@@ -31,7 +32,7 @@ def images_to_landmarks(image_paths: List[str], model_class: ModelClass):
 
 def create_data_dataset(model_class: ModelClass, image_dataset_path):
     image_paths = glob.glob(os.path.join(image_dataset_path, '*'))
-    images_to_landmarks(image_paths, model_class)
+    images_to_data(image_paths, model_class)
 
 
 def main():
